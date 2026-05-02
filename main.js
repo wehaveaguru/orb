1aa124a7c1e25fe5a01f829ebdcbc23d89b246bd
Guru <207536326+wehaveaguru@users.noreply.github.com>
2026-05-03 00:17:26 +0530

    Changes made

--- main.js
+++ main.js
@@ -12,4 +12,9 @@ client.once('ready', () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.on('message',ms=>{
+    if (ms.content=='ping'){
+        ms.reply('pong');
+    }
+})
+
